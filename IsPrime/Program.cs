b343ed0b2a7237e102d9a32f25a2b2b644a12b7b
atmar <nathaniel.atmar@outlook.com>
2019-08-08 10:03:14 -0700

    made a "faster" prime checker by filtering stuff out

--- IsPrime/Program.cs
+++ IsPrime/Program.cs
@@ -6,20 +6,31 @@ namespace IsPrime
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(IsPrime(0));
-            Console.WriteLine(IsPrime(1));
-            Console.WriteLine(IsPrime(2));
-            Console.WriteLine(IsPrime(3));
-            Console.WriteLine(IsPrime(4));
-            Console.WriteLine(IsPrime(5));
-            Console.WriteLine(IsPrime(6));
-            Console.WriteLine(IsPrime(7));
-            Console.WriteLine(IsPrime(8));
+            Console.WriteLine(IsPrimeFaster(0));
+            Console.WriteLine(IsPrimeFaster(1));
+            Console.WriteLine(IsPrimeFaster(2));
+            Console.WriteLine(IsPrimeFaster(3));
+            Console.WriteLine(IsPrimeFaster(4));
+            Console.WriteLine(IsPrimeFaster(5));
+            Console.WriteLine(IsPrimeFaster(6));
+            Console.WriteLine(IsPrimeFaster(7));
+            Console.WriteLine(IsPrimeFaster(8));
         }
         public static bool IsPrime(int n)
         {
             if (n < 2) return false;
             for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+        public static bool IsPrimeFaster(int n)
+        {
+            if (n < 2) return false;
+            if (n < 4) return true;
+            if (n % 2 == 0 || n % 3 == 0) return false;
+            for (int i = 3; i < n; i += 2)
             {
                 if (n % i == 0) return false;
             }
