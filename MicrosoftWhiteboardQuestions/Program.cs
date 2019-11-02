24dfb602460fbf25ad284fe690084d429e7aa9b3
Nathaniel Atmar <Nathaniel.Atmar@outlook.com>
2019-11-02 01:23:59 -0700

    clock problem

--- MicrosoftWhiteboardQuestions/Program.cs
+++ MicrosoftWhiteboardQuestions/Program.cs
@@ -11,6 +11,7 @@ namespace MicrosoftWhiteboardQuestions
     {
         static void Main(string[] args)
         {
+            ClockAngle(7, 00);
             int[] arr = FrontZeros(new int[]{ 1, 0, 4, 3, 0, 2, 7, 0});
             foreach (var v in arr)
             {
@@ -21,6 +22,9 @@ namespace MicrosoftWhiteboardQuestions
         //Using a hash table, take a string and count up the number of occurances of each character, casing does not matter. If there are no characters, return a zero. Only a-z
         public static Dictionary<char, int> HashCounter(string str)
         {
+            /*
+             do validation here
+             */
             Dictionary<char, int> AlphabetSet= new Dictionary<char, int>();
 
             //stuff goes here
@@ -31,6 +35,9 @@ namespace MicrosoftWhiteboardQuestions
         //Given a string and a char array, split into a list the string based on the characters in the array.
         public static List<string> Splitter(string str, string[] strArr)
         {
+            /*
+             do validation here
+             */
             List<string> ret = new List<string>();
 
             //stuff goes here
@@ -38,17 +45,36 @@ namespace MicrosoftWhiteboardQuestions
             return ret;
         }
 
-        //Given the hours and minutes on a clock, return the shortest angle between them (in degrees)
-        public static int ClockAngle(int hour, int minute)
+        //Given the hours and minutes on a clock, get the shortest angle between them (in degrees)
+        public static void ClockAngle(int hour, int minute)
         {
-            int angle = 0;
+            while (hour > 11)
+            {
+                hour -= 12;
+            }
 
-            return angle;
+            while (minute > 59)
+            {
+                minute -= 59;
+            }
+
+
+            int angle = Math.Abs((minute * 6) - (hour * 30));
+
+            if (angle > 180)
+            {
+                angle = 360 - angle;
+            }
+
+            Console.WriteLine(angle + " degrees");
         }
 
         //Given an array of numbers, move all of the 0s to the front, while maintaining order on the rest
         public static int[] FrontZeros(int[] numList)
         {
+            /*
+             do validation here
+             */
             int[] newList = new int[numList.Length];
             int zeroIndex = 0;
             int otherIndex = numList.Length - 1;
